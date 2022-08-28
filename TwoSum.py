9407f9f1ad0efad25a691ca2b97af30ee2911fe5
Nyan Myo Sett <nyanmyosettnms@gmail.com>
2022-08-28 22:53:49 +0630

    Add my first solution

---
+++ TwoSum.py
@@ -0,0 +1,17 @@
+from curses.ascii import SO
+
+
+class Solution:
+    def twoSum(self, nums: list[int], target: int) -> list[int]:
+        prevMap = {}  # val -> index
+        
+        for i, n in enumerate(nums):
+            diff = target - n
+            prevMap[n] = i
+            if diff in prevMap:
+                return [prevMap[diff], i]
+            
+
+if __name__ == '__main__':
+    s = Solution()
+    print(s.twoSum([2,7,11,15], 9))
